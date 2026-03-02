3c0ae6cbace0a7b4217c6374b49644e0564d399b
gandoalex <gandoalex73@gmail.com>
2026-03-02 17:23:49 +0100

    feat: skyboard for docker

---
+++ README.md
@@ -0,0 +1,7 @@
+# Skyboard
+
+Petite application Node.js Express pour tests CI/CD et déploiement Ubuntu.
+
+## Lancer en local
+npm install
+npm start
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur skyboard 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Skyboard tourne sur http://localhost:${PORT}`);
+});
